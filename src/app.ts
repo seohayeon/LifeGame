e5527f94da1dd63e9cb930f558cc15535c84b86d
seohayeon <ondojung@naver.com>
2021-09-15 13:10:00 +0900

    Chore:app use router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import userRouter from "./router/users";
+import mineRouter from "./router/mine";
+import fishingRouter from "./router/fishing";
+import orchardRouter from "./router/orchard";
+
 import db from './models/index'
 import * as bodyParser from 'body-parser';
 db();
@@ -12,9 +16,13 @@ app.use(
   }),
 );
 
-const port:number = 3000
+const port:number = 3002
 
 app.use("/users", userRouter);
+app.use("/fishing", fishingRouter);
+app.use("/mine", mineRouter);
+app.use("/orchard", orchardRouter);
+
 
 app.listen(port, () => {
   console.log(`${port}에서 인생게임 서버를 열었습니다.`);
